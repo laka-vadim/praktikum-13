b6b4b7ad8239da1b33832dbeb5488b1580601540
Vadim Popov <vadim@laka-web.dev>
2019-11-17 00:41:24 +0700

    auth added

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,24 @@
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/users');
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.id)
     .then((user) => {
       if (!user) {
-        res.status(404).send({ message: 'Пользователь не найден' });
+        res.status(404).send({ message: 'Err 404: User not found' });
         return;
       }
       res.send({ data: user });
     })
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка на сервере: ${err}` }));
+    .catch((err) => res.status(400).send({ message: `Err 400: Incorrect data. Server answer: ${err}` }));
 };
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка на сервере: ${err}` }));
+    .catch((err) => res.status(400).send({ message: `Err 400: Incorrect data. Server answer: ${err}` }));
 };
 
 module.exports.postUser = (req, res) => {
@@ -32,14 +33,44 @@ module.exports.postUser = (req, res) => {
     res.status(400).send({ message: 'Err 400: Invalid email' });
     return;
   }
-  const hash = bcrypt.hash(password, 10);
-  User.create({
-    email,
-    hash,
-    name,
-    about,
-    avatar,
-  })
-    .then((user) => res.status(201).send({ data: user }))
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      User.create({
+        email,
+        password: hash,
+        name,
+        about,
+        avatar,
+      })
+        .then((user) => res.status(201).send({ data: user }))
+        .catch((err) => res.status(400).send({ message: `Err 400: Incorrect data. Server answer: ${err}` }));
+    })
     .catch((err) => res.status(400).send({ message: `Err 400: Incorrect data. Server answer: ${err}` }));
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Err 401: Incorrect login or/and email.'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((match) => {
+          if (!match) {
+            return Promise.reject(new Error('Err 401: Incorrect login or/and email.'));
+          }
+          const token = jwt.sign(
+            { _id: user._id },
+            'some-secret-key',
+            { expiresIn: '7d' },
+          );
+          return res
+            .cookie('jwt', token, {
+              maxAge: 3600000 * 24 * 7,
+              httpOnly: true,
+            }).end();
+        });
+    })
+    .catch((err) => res.status(401).send({ message: `${err.message}` }));
 };
