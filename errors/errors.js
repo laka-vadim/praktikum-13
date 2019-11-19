dc31c983554734bd8f911b1fe76b22d9771d176c
Vadim Popov <vadim@laka-web.dev>
2019-11-19 21:07:05 +0700

    add errors classes

---
+++ errors/errors.js
@@ -0,0 +1,22 @@
+class NotFoundError extends Error {
+  constructor(message) {
+      super(message);
+      this.statusCode = 404;
+  }
+}
+
+class UnauthorizedError extends Error {
+  constructor(message) {
+      super(message);
+      this.statusCode = 401;
+  }
+}
+
+class BadRequestError extends Error {
+  constructor(message) {
+      super(message);
+      this.statusCode = 400;
+  }
+}
+
+module.exports = { NotFoundError, UnauthorizedError, BadRequestError };
